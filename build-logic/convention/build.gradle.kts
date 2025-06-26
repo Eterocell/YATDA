459849d259752a2f5c7c3387a9171a4581700ee4
Eterocell <eterocell@outlook.com>
2025-06-26 10:20:51 +0800

    fix(deps): remove unnecessary kotlin stdlib dependency
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -11,8 +11,6 @@ dependencies {
     implementation(libs.gradle.plugin.hilt)
     implementation(libs.gradle.plugin.dependency.guard)
 
-    // TODO: Switch back to kotlin 2.2.0 after gradle supports it
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
     implementation(libs.gradle.plugin.spotless)
     implementation(libs.gradle.plugin.kotlin)
     implementation(libs.gradle.plugin.compose.compiler)
