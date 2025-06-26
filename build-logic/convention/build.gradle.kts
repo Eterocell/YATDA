969b301f8005363c17fd1fcd2b446cf21fd0ca46
Eterocell <eterocell@outlook.com>
2025-06-26 10:15:20 +0800

    fix(deps): kotlin 2.2.0, update dependencies
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -11,7 +11,8 @@ dependencies {
     implementation(libs.gradle.plugin.hilt)
     implementation(libs.gradle.plugin.dependency.guard)
 
-    implementation(libs.kotlin.stdlib)
+    // TODO: Switch back to kotlin 2.2.0 after gradle supports it
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.21")
     implementation(libs.gradle.plugin.spotless)
     implementation(libs.gradle.plugin.kotlin)
     implementation(libs.gradle.plugin.compose.compiler)
