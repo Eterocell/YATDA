38cd728de37eb718a52c36fad2aced24f1813177
Eterocell <eterocell@outlook.com>
2025-11-20 10:09:44 +0800

    chore(deps): update dependencies
    Signed-off-by: Eterocell <eterocell@outlook.com>

--- build-logic/convention/src/main/kotlin/build-logic.root-project.gradle.kts
+++ build-logic/convention/src/main/kotlin/build-logic.root-project.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 val wrapper: Wrapper by tasks.named<Wrapper>("wrapper") {
-    gradleVersion = "9.2.0"
+    gradleVersion = "9.2.1"
     distributionType = Wrapper.DistributionType.ALL
     val sha256 =
         URI
